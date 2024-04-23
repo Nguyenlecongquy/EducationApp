418dae6b016e089c9087ae1fd539f2b4a73ff098
NguyenLeCongQuy <nguyenlecongquy@gmail.com>
2024-04-23 21:15:01 +0700

    Update AuthContext.js, eas.json, package.json, app.json, and Login.js files

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,88 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Image, View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Image, View, Text, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 
 const Login = () => {
   const image = require("../assets/login-image.png");
+  const [userInfo, setUserInfo] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+
+  const configureGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId:
+        "174403152828-8vra1prgjgnm5s3u5h49gdke1ifspdqp.apps.googleusercontent.com",
+      androidClientId:
+        "174403152828-78ol49k6sn8f1ecukapkbnp3ctmi3jcl.apps.googleusercontent.com",
+      iosClientId:
+        "174403152828-v56nlr8te7ileq1rnaja8jfljm1hoscd.apps.googleusercontent.com",
+    });
+  };
+
+  const getUserData = async (accessTokenValue) => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: {
+            Authorization: `Bearer ${accessTokenValue}`,
+          },
+        }
+      );
+      return response.json();
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    configureGoogleSignIn();
+  }, []);
+
+  const signIn = async () => {
+    console.log("pressed sign in");
+    try {
+      await GoogleSignin.hasPlayServices();
+      const result = await GoogleSignin.signIn();
+      console.log("userInfo", result);
+      //   const data = await GoogleSignin.getTokens();
+      console.log("token", data);
+    //   setAccessToken(data.accessToken);
+
+      setUserInfo(userInfo);
+    } catch (error) {
+      switch (error.code) {
+        case statusCodes.IN_PROGRESS:
+          console.log("in progress");
+          break;
+        case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+          console.log("play services not available");
+          break;
+        case statusCodes.SIGN_IN_CANCELLED:
+          console.log("sign in cancelled");
+          break;
+        default:
+          console.log("Something went wrong", error);
+          break;
+      }
+    }
+  };
+
+  const signOut = async () => {
+    console.log("pressed sign out");
+    try {
+      await GoogleSignin.revokeAccess();
+      await GoogleSignin.signOut();
+      setUserInfo(null);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="h-1/3 w-full">
@@ -15,12 +92,21 @@ const Login = () => {
         <Text className="text-center font-bold text-2xl">
           Welcome to E-learning
         </Text>
-        <Text className="text-center mt-20 text-base font-bold">Login/Signup</Text>
+        <Text className="text-center mt-20 text-base font-bold">
+          Login/Signup
+        </Text>
+        <View className="flex flex-row justify-center items-center w-full mt-8">
+          <GoogleSigninButton
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={signIn}
+          />
+        </View>
+        <Pressable onPress={signOut}>
+          <Text>Sign Out</Text>
+        </Pressable>
       </View>
-      <View className="flex flex-row bg-primary p-3 m-8 justify-center items-center rounded-xl">
-        <Ionicons name="logo-google" size={24} color="white" />
-        <Text className="text-base ml-2 text-text-white">Sign in with Google</Text>
-      </View>
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
